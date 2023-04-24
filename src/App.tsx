4a3d17011133991529bfaad9a258bef0ebd122b5
Ajay Shrestha <er.ajaystha@gmail.com>
2023-04-24 18:51:29 -0400

    Main Layout integration

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import MainLayout from "home/MainLayout";
+
+function App() {
+  return <MainLayout />;
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: "main-app",
+      remotes: {
+        home: "http://localhost:3000/assets/remoteEntry.js",
+      },
+      shared: ["react", "react-dom", "react-router-dom"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
