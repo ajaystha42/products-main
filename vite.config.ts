9217998668a77e69205d7f3fcf3eddd4dc89af0a
Ajay Shrestha <er.ajaystha@gmail.com>
2023-04-24 21:42:08 -0400

    env config

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,26 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: "main-app",
-      remotes: {
-        home: "http://localhost:3000/assets/remoteEntry.js",
-      },
-      shared: ["react", "react-dom", "react-router-dom"],
-    }),
-  ],
-  build: {
-    modulePreload: false,
-    target: "esnext",
-    minify: false,
-    cssCodeSplit: false,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [
+      react(),
+      federation({
+        name: "main-app",
+        remotes: {
+          home: env["VITE_HOME_URL"],
+        },
+        shared: ["react", "react-dom", "react-router-dom"],
+      }),
+    ],
+    build: {
+      modulePreload: false,
+      target: "esnext",
+      minify: false,
+      cssCodeSplit: false,
+    },
+  };
 });
